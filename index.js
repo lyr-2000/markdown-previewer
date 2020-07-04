20706a50e79289fd2729eda228127a23de2bcf73
Youness BENNAJ <youness.bennaj@gmail.com>
2020-07-04 17:54:44 +0200

    finish style

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-//When I enter text into the #editor element, the #preview element is updated as 
-//I type to display the content of the textarea => Ok
-
-
-// GitHub flavored markdown into #editor => rendered as HTML in the #preview element
-//using https://github.com/markedjs/marked => Ok
-
-//The default text in the #editor: 
-//a header (H1 size), a sub header (H2 size), a link, inline code, 
-//a code block, a list item, a blockquote, an image, and bolded text. => OK
-
-
 const placeholder = `
 # Markdown Previewer with Jquery
 
@@ -36,15 +24,6 @@ There's also [links](https://www.facebook.com)
 3. Item 3
 `
 
-//events =>  
-//change => input value change
-//keyup => keyborad key up
-//paste => paste content in textarea
-$("#editor").on("change keyup paste", function () {
-    var currentVal = $(this).val();
-    $("#preview").html(convertMarkdownToHTML(currentVal));
-});
-
 function convertMarkdownToHTML(markdown) {
     return marked(markdown);
 }
@@ -56,6 +35,15 @@ function setDefaultTextOnLoad() {
 
 //!!! => onload() deprecated
 $(window).on('load', function () {
+    //events =>  
+    //change => input value change
+    //keyup => keyborad key up
+    //paste => paste content in textarea
+    $("#editor").on("change keyup paste", function () {
+        var currentVal = $(this).val();
+        $("#preview").html(convertMarkdownToHTML(currentVal));
+    });
+
     setDefaultTextOnLoad();
 });
 
