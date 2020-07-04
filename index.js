6779d6938e1b51cc1bb0821faba6aa2408d88ac2
Youness BENNAJ <youness.bennaj@gmail.com>
2020-07-04 16:03:10 +0200

    add event handler on textarea change event with keyup event and paste event then add the current value of the input to the #preview element

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Markdown Preview');
+//When I enter text into the #editor element, the #preview element is updated as 
+//I type to display the content of the textarea
+
+
+// GitHub flavored markdown into #editor => rendered as HTML in the #preview element
+//using https://github.com/markedjs/marked
+
+//The default text in the #editor: 
+//a header (H1 size), a sub header (H2 size), a link, inline code, 
+//a code block, a list item, a blockquote, an image, and bolded text.
+
+//events =>  
+//change => input value change
+//keyup => keyborad key up
+//paste => paste content in textarea
+$("#editor").on("change keyup paste", function () {
+    var currentVal = $(this).val();
+    $("#preview").html(currentVal);
+});
+
+
+
+//!!! => onload() deprecated
+$(window).on('load', function () {
+    console.log("loaded...");
+});
+
